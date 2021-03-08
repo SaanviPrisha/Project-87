a5f7924a626855612681a5187da3b5df921855e8
SaanviPrisha <69468899+SaanviPrisha@users.noreply.github.com>
2021-03-07 21:28:50 -0500

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,12 +1,69 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { DrawerItems } from 'react-navigation-drawer'
+import { Avatar } from 'react-native-elements'
+import * as ImagePicker from 'expo-image-picker'
+import * as Permissons from 'expo-permissions'
+import db from '../Config'
 import firebase from 'firebase'
 
 export default class Sidebar extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      image: '#',
+      userId: firebase.auth().currentUser.email
+    }
+  }
+  selectPicture = async () => {
+    const { cancel, uri } = await ImagePicker.launchImageLibraryAsync({ 
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4,3],
+      quality: 1
+     })
+     if(!cancel) {
+       this.uploadImage(uri,this.state.userId)
+     } 
+  }
+  uploadImage = async (uri,email) => {
+    var response = await fetch(uri)
+    var blob = await response.blob()
+    var ref = firebase.storage().ref().child("user_profiles/" + email)
+    return( ref.put(blob).then(response => {
+      this.fetchImage(email)
+    }) )
+  }
+  fetchImage = (email) => {
+    var ref = firebase.storage().ref().child("user_profiles/" + email)
+    ref.getDownloadURL().then(uri => {
+      this.setState({
+        image: uri
+      })
+    }).catch(error => {
+      this.setState({
+        image: '#'
+      })
+    })
+  }
+  componentDidMount() {
+    this.fetchImage(this.state.userId)
+  }
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.center}>
+          <Avatar 
+            rounded
+            showEditButton
+            source = {{uri: this.state.image }}
+            size = "medium"
+            containerStyle = {styles.avatar}
+            onPress = {() => {
+              this.selectPicture()
+            }}
+          />
+        </View>
         <View style={styles.center}>
             <DrawerItems 
                 {...this.props}
@@ -45,5 +102,12 @@ const styles = StyleSheet.create({
     fontSize: 15, 
     fontWeight: 'bold',
     alignSelf: 'center',
+  },
+  avatar: {
+    width: '40%',
+    height: '20%',
+    margin: 20,
+    borderRadius: 30,
+    alignSelf: 'center'
   },
 });
